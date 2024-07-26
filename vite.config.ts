344e6c281d66a34c93aea2885d6958349209e8da
Nikadfafe <b1ffstarzv@rambler.ru>
2024-07-26 19:12:59 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'ethers'],
+                    walletconnect: ['@walletconnect/ethereum-provider', '@walletconnect/modal'],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Увеличиваем лимит предупреждения до 1000 кБ
+    },
+    optimizeDeps: {
+        include: ['react', 'react-dom', 'ethers', '@walletconnect/ethereum-provider', '@walletconnect/modal'],
+    },
 });
